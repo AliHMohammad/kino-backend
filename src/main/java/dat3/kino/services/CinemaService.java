10c38415108a06e844d57d31ee2ed19790dc0399
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-03-12 12:54:34 +0100

    Added CinemaService with create, read, readAll and updatePartially methods

---
+++ src/main/java/dat3/kino/services/CinemaService.java
@@ -0,0 +1,45 @@
+package dat3.kino.services;
+
+import dat3.kino.entities.Cinema;
+import dat3.kino.exception.EntityNotFoundException;
+import dat3.kino.repositories.CinemaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CinemaService {
+    private final CinemaRepository cinemaRepository;
+
+    public CinemaService(CinemaRepository cinemaRepository) {
+        this.cinemaRepository = cinemaRepository;
+    }
+
+    public Cinema createCinema(Cinema newCinema) {
+        return cinemaRepository.save(newCinema);
+    }
+
+    public Cinema readCinema(Long id) {
+        return cinemaRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Cinema", id));
+    }
+
+    public List<Cinema> readAllCinemas() {
+
+        return cinemaRepository.findAll();
+    }
+
+    // - updateCinema PATCH
+    public Cinema updateCinemaPartially (Long id, Cinema cinema) {
+        Cinema cinemaToUpdate = cinemaRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Cinema", id));
+        if (cinema.getName() != null) {
+            cinemaToUpdate.setName(cinema.getName());
+        }
+        if (cinema.getCity() != null) {
+            cinemaToUpdate.setCity(cinema.getCity());
+        }
+        if (cinema.getIsActive() !=null) {
+            cinemaToUpdate.setIsActive(cinema.getIsActive());
+        }
+        return cinemaRepository.save(cinemaToUpdate);
+    }
+}
