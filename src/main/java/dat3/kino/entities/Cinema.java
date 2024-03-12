fd8cf1d2a77cf60c409c856488069a1e535a3185
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-03-12 13:00:17 +0100

    removed unused import

--- src/main/java/dat3/kino/entities/Cinema.java
+++ src/main/java/dat3/kino/entities/Cinema.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
