dd72059911c4085b8b6bb19282a3999678ed8268
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-03-13 13:45:36 +0100

    KINO-68 added if statements so creation does not run unless db is empty

--- src/main/java/dat3/kino/configuration/SetupCinemaData.java
+++ src/main/java/dat3/kino/configuration/SetupCinemaData.java
@@ -13,8 +13,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
 
 @Component
 public class SetupCinemaData implements ApplicationRunner {
@@ -34,35 +32,16 @@ public SetupCinemaData(CinemaService cinemaService, AuditoriumService auditorium
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("Application is running");
         init();
-
     }
 
     private void init() {
-        // initCinemas
         Cinema cinema1 = new Cinema("Empire Bio", "Copenhagen", true);
         Cinema cinema2 = new Cinema("Cinemaxx", "Aarhus", true);
-
-        System.out.println("Creating cinemas");
-
-        cinemaService.createCinema(cinema1);
-        cinemaService.createCinema(cinema2);
-
-        // initSeatPricing
-        System.out.println("Creating seat pricing");
-        System.out.println("------------------------------------");
 
         SeatPricing cowboy = new SeatPricing("cowboy", 50);
         SeatPricing standard = new SeatPricing("standard", 75);
         SeatPricing deluxe = new SeatPricing("deluxe", 100);
 
-        seatPricingService.createSeatPricing(cowboy);
-        seatPricingService.createSeatPricing(standard);
-        seatPricingService.createSeatPricing(deluxe);
-
-
-        System.out.println("Creating auditoriums");
-        System.out.println("------------------------------------");
-        // initAuditoriums
         Auditorium auditorium1 = new Auditorium("Sal 1", cinema1);
         Auditorium auditorium2 = new Auditorium("Sal 2", cinema1);
         Auditorium auditorium3 = new Auditorium("Sal 1", cinema2);
@@ -70,31 +49,48 @@ public SetupCinemaData(CinemaService cinemaService, AuditoriumService auditorium
         Auditorium auditorium5 = new Auditorium("Sal 3", cinema2);
         Auditorium auditorium6 = new Auditorium("Sal 4", cinema2);
 
-        auditoriumService.createAuditorium(auditorium1, 20, 12);
-        auditoriumService.createAuditorium(auditorium2, 24, 16);
-        auditoriumService.createAuditorium(auditorium3, 22, 12);
-        auditoriumService.createAuditorium(auditorium4, 24, 12);
-        auditoriumService.createAuditorium(auditorium5, 20, 14);
-        auditoriumService.createAuditorium(auditorium6, 25, 16);
+        Movie movie1 = new Movie(693134L, "Dune Part Two", "/5aUVLiqcW0kFTBfGsCWjvLas91w.jpg", 170, LocalDate.of(2024, 2, 27));
+        Movie movie2 = new Movie(1011985L, "Kung Fu Panda 4", "/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg", 90, LocalDate.of(2024, 3, 2));
+        Movie movie3 = new Movie(1119544L, "Rom", "/vdex5zUokgWTnsE4iZt9BTWpNdM.jpg", 98, LocalDate.of(2024, 2, 22));
+        Movie movie4 = new Movie(802219L, "Bob Marley: One Love", "/mKWalirPreEdCKDJjc5TKeOP2xi.jpg", 104, LocalDate.of(2024, 2, 14));
+        Movie movie5 = new Movie(1078249L, "Den grænseløse", "/1T1nn9fAb1N46knsqaBiIpxm4M2.jpg", 132, LocalDate.of(2024, 2, 1));
 
+        // initCinemas
+        if (cinemaService.readAllCinemas().isEmpty()) {
+            System.out.println("Creating cinemas");
+
+            cinemaService.createCinema(cinema1);
+            cinemaService.createCinema(cinema2);
+        }
+        // initSeatPricing
+        if (seatPricingService.readAllSeatPricing().isEmpty()) {
+            System.out.println("Creating seat pricing");
+
+            seatPricingService.createSeatPricing(cowboy);
+            seatPricingService.createSeatPricing(standard);
+            seatPricingService.createSeatPricing(deluxe);
+        }
+        // initAuditoriums
+        if (auditoriumService.readAllAuditoriums().isEmpty()) {
+            System.out.println("Creating auditoriums");
+
+            auditoriumService.createAuditorium(auditorium1, 20, 12);
+            auditoriumService.createAuditorium(auditorium2, 24, 16);
+            auditoriumService.createAuditorium(auditorium3, 22, 12);
+            auditoriumService.createAuditorium(auditorium4, 24, 12);
+            auditoriumService.createAuditorium(auditorium5, 20, 14);
+            auditoriumService.createAuditorium(auditorium6, 25, 16);
+        }
 
         // initMovies
-        System.out.println("Creating movies");
-        System.out.println("------------------------------------");
-        Movie movie1 = new Movie("Dune Part Two", "Dune poster", 170, new Date(), LocalDateTime.now());
-        Movie movie2 = new Movie("Kung Fu Panda", "Kung Fu Poster", 90, new Date(), LocalDateTime.now());
-        Movie movie3 = new Movie("Rom", "Rom poster", 98, new Date(), LocalDateTime.now());
-        Movie movie4 = new Movie("Bob Marley: One Love", "Bob Marley poster", 104, new Date(), LocalDateTime.now());
-        Movie movie5 = new Movie("Den grænseløse", "Den grænseløse poster", 132, new Date(), LocalDateTime.now());
-         movieService.createMovie(movie1);
-         movieService.createMovie(movie2);
-         movieService.createMovie(movie3);
-         movieService.createMovie(movie4);
-         movieService.createMovie(movie5);
+        if(movieService.readAllMovies().isEmpty()) {
+            System.out.println("Creating movies");
 
-
-
+            movieService.createMovie(movie1);
+            movieService.createMovie(movie2);
+            movieService.createMovie(movie3);
+            movieService.createMovie(movie4);
+            movieService.createMovie(movie5);
+        }
     }
-
-
 }
