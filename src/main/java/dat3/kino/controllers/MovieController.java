7e70b000ab9247372b7813d6d1af0bc7c4451412
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-03-14 08:48:43 +0100

    KINO-68 changed return types to dtos for MovieController and MovieService

--- src/main/java/dat3/kino/controllers/MovieController.java
+++ src/main/java/dat3/kino/controllers/MovieController.java
@@ -26,6 +26,7 @@ public class MovieController {
         return ResponseEntity.ok(movieService.readAllMovies());
     }
 
+
     @GetMapping("/movies/TMDB/{id}")
     public ResponseEntity<MovieDetailsResponse> getTMDBMovie(@PathVariable Integer id) {
         return ResponseEntity.ok(tmdbService.findMovieFromTMDB(id));
